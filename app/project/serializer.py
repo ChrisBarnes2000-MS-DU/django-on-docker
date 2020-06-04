2e2172a381470f96a494349891b4b9c79cd1cb48
Christopher Barnes <chris.barnes.2000@me.com>
2020-06-04 16:32:26 -0700

    [update] hello_django to project folder separated api from urls

--- app/project/serializer.py
+++ app/project/serializer.py
@@ -1,13 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
-
 from django.contrib.auth.models import User, Group
 # from users.models import CustomUser as User
 from rest_framework import routers, serializers, viewsets, permissions
-# from Our_API.views import *
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -21,6 +14,7 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ['url', 'name']
 
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -28,6 +22,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
@@ -41,25 +36,3 @@ class GroupViewSet(viewsets.ModelViewSet):
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
-
-from upload.views import image_upload
-
-urlpatterns = [
-    path("", image_upload, name="upload"),
-
-    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
-    path("ACP/", admin.site.urls),
-
-    # API
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
-
-if bool(settings.DEBUG):
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns = [
-#         path('__debug__/', include(debug_toolbar.urls)),
-#     ] + urlpatterns
---
+++ app/project/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+
+
+from upload.views import image_upload
+from .serializer import router
+
+urlpatterns = [
+    path("", image_upload, name="upload"),
+
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path("ACP/", admin.site.urls),
+
+    # API
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns = [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ] + urlpatterns
