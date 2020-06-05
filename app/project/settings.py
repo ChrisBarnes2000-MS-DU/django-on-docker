36797d129b67353da9e8f904b09399e2eccc0301
Christopher Barnes <chris.barnes.2000@me.com>
2020-06-04 17:04:52 -0700

    [add] allauth

--- app/project/settings.py
+++ app/project/settings.py
@@ -24,8 +24,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    # 'django.contrib.sites',
-    # 'django.contrib.postgres',
+    'django.contrib.sites',
+    'django.contrib.postgres',
 
     "rest_framework",
 
@@ -35,7 +35,95 @@ INSTALLED_APPS = [
 
 
 
-    
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # ... include the providers you want to enable:
+    # 'allauth.socialaccount.providers.agave',
+    # 'allauth.socialaccount.providers.amazon',
+    # 'allauth.socialaccount.providers.angellist',
+    # 'allauth.socialaccount.providers.asana',
+    # 'allauth.socialaccount.providers.auth0',
+    # 'allauth.socialaccount.providers.authentiq',
+    # 'allauth.socialaccount.providers.baidu',
+    # 'allauth.socialaccount.providers.basecamp',
+    # 'allauth.socialaccount.providers.bitbucket',
+    # 'allauth.socialaccount.providers.bitbucket_oauth2',
+    # 'allauth.socialaccount.providers.bitly',
+    # 'allauth.socialaccount.providers.cern',
+    # 'allauth.socialaccount.providers.coinbase',
+    # 'allauth.socialaccount.providers.dataporten',
+    # 'allauth.socialaccount.providers.daum',
+    # 'allauth.socialaccount.providers.digitalocean',
+    # 'allauth.socialaccount.providers.discord',        --
+    # 'allauth.socialaccount.providers.disqus',
+    # 'allauth.socialaccount.providers.douban',
+    # 'allauth.socialaccount.providers.draugiem',
+    # 'allauth.socialaccount.providers.dropbox',
+    # 'allauth.socialaccount.providers.dwolla',
+    # 'allauth.socialaccount.providers.edmodo',
+    # 'allauth.socialaccount.providers.edx',
+    # 'allauth.socialaccount.providers.eveonline',
+    # 'allauth.socialaccount.providers.evernote',
+    # 'allauth.socialaccount.providers.exist',
+    'allauth.socialaccount.providers.facebook',
+    # 'allauth.socialaccount.providers.feedly',
+    # 'allauth.socialaccount.providers.fivehundredpx',
+    # 'allauth.socialaccount.providers.flickr',
+    # 'allauth.socialaccount.providers.foursquare',
+    # 'allauth.socialaccount.providers.fxa',
+    'allauth.socialaccount.providers.github',
+    # 'allauth.socialaccount.providers.gitlab',
+    'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.hubic',
+    'allauth.socialaccount.providers.instagram',
+    # 'allauth.socialaccount.providers.jupyterhub',
+    # 'allauth.socialaccount.providers.kakao',
+    # 'allauth.socialaccount.providers.keycloak',
+    # 'allauth.socialaccount.providers.line',
+    # 'allauth.socialaccount.providers.linkedin',       --
+    # 'allauth.socialaccount.providers.linkedin_oauth2',
+    # 'allauth.socialaccount.providers.mailru',
+    # 'allauth.socialaccount.providers.mailchimp',
+    # 'allauth.socialaccount.providers.meetup',
+    # 'allauth.socialaccount.providers.microsoft',
+    # 'allauth.socialaccount.providers.mixer',
+    # 'allauth.socialaccount.providers.naver',
+    # 'allauth.socialaccount.providers.nextcloud',
+    # 'allauth.socialaccount.providers.odnoklassniki',
+    # 'allauth.socialaccount.providers.openid',
+    # 'allauth.socialaccount.providers.openstreetmap',
+    # 'allauth.socialaccount.providers.orcid',
+    # 'allauth.socialaccount.providers.paypal',         --
+    # 'allauth.socialaccount.providers.patreon',
+    # 'allauth.socialaccount.providers.persona',
+    # 'allauth.socialaccount.providers.pinterest',
+    # 'allauth.socialaccount.providers.reddit',
+    # 'allauth.socialaccount.providers.robinhood',
+    # 'allauth.socialaccount.providers.sharefile',
+    # 'allauth.socialaccount.providers.shopify',
+    # 'allauth.socialaccount.providers.slack',          --
+    # 'allauth.socialaccount.providers.soundcloud',
+    # 'allauth.socialaccount.providers.spotify',
+    # 'allauth.socialaccount.providers.stackexchange',
+    # 'allauth.socialaccount.providers.steam',
+    # 'allauth.socialaccount.providers.strava',
+    # 'allauth.socialaccount.providers.stripe',
+    # 'allauth.socialaccount.providers.trello',
+    # 'allauth.socialaccount.providers.tumblr',         --
+    # 'allauth.socialaccount.providers.twentythreeandme',
+    # 'allauth.socialaccount.providers.twitch',
+    # 'allauth.socialaccount.providers.twitter',
+    # 'allauth.socialaccount.providers.untappd',
+    # 'allauth.socialaccount.providers.vimeo',
+    # 'allauth.socialaccount.providers.vimeo_oauth2',
+    # 'allauth.socialaccount.providers.vk',
+    # 'allauth.socialaccount.providers.weibo',
+    # 'allauth.socialaccount.providers.weixin',
+    # 'allauth.socialaccount.providers.windowslive',
+    # 'allauth.socialaccount.providers.xing',
+    # 'allauth.socialaccount.providers.yandex',
+    # 'allauth.socialaccount.providers.ynab',
 ]
 
 
@@ -109,6 +197,7 @@ LOGOUT_REDIRECT_URL = "/"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 
+SITE_ID = 1
 
 ADMIN_HONEYPOT_EMAIL_ADMINS = True
 ADMINS = [('Chris Barnes', 'chris.barnes.2000@me.com')]
@@ -118,7 +207,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
-    # 'allauth.account.auth_backends.AuthenticationBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 )
 
 
